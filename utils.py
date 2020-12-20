accfbf24aab95b9eaa9c067ec8a6dbb77044d0c6
dayyass <dayyass@yandex.ru>
2020-12-20 22:25:11 +0300

    add shap

--- utils.py
+++ utils.py
@@ -1,10 +1,21 @@
 import os
+import shap
 import torch
 import random
 import numpy as np
 from tqdm import tqdm
 from PIL import Image
 from typing import Tuple
+
+
+def set_global_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
 
 
 def load_images(path: str, verbose: bool = True) -> Tuple[np.ndarray, np.ndarray]:
@@ -34,10 +45,16 @@ def load_images(path: str, verbose: bool = True) -> Tuple[np.ndarray, np.ndarray
     return images, labels
 
 
-def set_global_seed(seed: int):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
+def shap_deep_explainer(
+        model: torch.nn.Module,
+        background: torch.Tensor,
+        test_images: torch.Tensor,
+):
+    e = shap.DeepExplainer(model, background)
+    shap_values = e.shap_values(test_images)
+
+    shap_numpy = [np.swapaxes(np.swapaxes(s, 1, -1), 1, 2) for s in shap_values]
+    test_numpy = np.swapaxes(np.swapaxes(test_images.cpu().numpy(), 1, -1), 1, 2)
+
+    # plot the feature attributions
+    shap.image_plot(shap_numpy, test_numpy)
